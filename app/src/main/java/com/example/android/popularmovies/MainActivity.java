4d91f7d3817c405a04cb7a99768ee69a99826bdd
Yukun Chen <ychen152@ucr.edu>
2016-12-22 20:13:32 -0800

    modify file name

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,15 +22,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public static final String LOG_TAG = MainActivity.class.getName();
 
     /*  fill out the API_KEY by yourself */
-    private static final String API_KEY ="ae3e5545ff8e675002634536af6905d4";
-
+    private static final String API_KEY ="";
 
     private static final String REQUEST_URL_POPULAR1 = "http://api.themoviedb.org/3/movie/";
+
     private static final String REQUEST_URL_POPULAR2 = "?language=zh&api_key=";
 
-    private static String REQUEST_URL_final;
-
-
+    private static String requestUrlFinal;
 
     private String original_order = "top_rated";
 
@@ -39,9 +38,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     private static final int Movie_LOADER_ID = 1;
 
     private MovieAdapter mAdapter;
-
-
-
 
 
     @Override
@@ -98,16 +94,25 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public Loader<List<MovieInfo>> onCreateLoader(int id, Bundle args) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 
-        return new MovieLoader(this, REQUEST_URL_final);
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+        original_order = orderBy;
+        requestUrlFinal = REQUEST_URL_POPULAR1 + orderBy  +REQUEST_URL_POPULAR2 + API_KEY;
+        return new MovieLoader(this, requestUrlFinal);
     }
 
     @Override
     public void onLoadFinished(Loader<List<MovieInfo>> loader, List<MovieInfo> data) {
         mAdapter.clear();
-
         if(data != null && !data.isEmpty()) {
             mAdapter.addAll(data);
+        }else{
+            // make Toast to show no Internet connection
+
         }
 
     }
@@ -116,8 +121,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public void onLoaderReset(Loader<List<MovieInfo>> loader) {
         mAdapter.clear();
     }
-
-
 
 
     @Override
@@ -136,10 +139,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             loader_count++;
             return;
         }
-
         loader_count++;
         original_order = orderBy;
-        REQUEST_URL_final = REQUEST_URL_POPULAR1 + orderBy  +REQUEST_URL_POPULAR2 + API_KEY;
+        requestUrlFinal = REQUEST_URL_POPULAR1 + orderBy  +REQUEST_URL_POPULAR2 + API_KEY;
         getLoaderManager().restartLoader(Movie_LOADER_ID, null, this);
 
     }
